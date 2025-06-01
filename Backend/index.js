8f4a531e3fadbe0f053b68415210fecc19fcc5f8
NikitaPoraiyar <poraiyarnikita@gmail.com>
2025-06-01 21:41:25 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -23,9 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 dotenv.config();
 // app.use(cors());
 app.use(cors({
-    origin: ['https://food-eg4entyfw-nikita-poraiyars-projects.vercel.app',
-        'https://admin-food-app.vercel.app'
-    ],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
